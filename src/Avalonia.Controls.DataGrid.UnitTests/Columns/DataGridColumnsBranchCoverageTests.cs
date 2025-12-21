89c42abb1f1ed2ec65d6e074fee269fa1f344acd
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-12-21 19:48:23 +0100

    Update DataGridColumnsBranchCoverageTests.cs

--- src/Avalonia.Controls.DataGrid.UnitTests/Columns/DataGridColumnsBranchCoverageTests.cs
+++ src/Avalonia.Controls.DataGrid.UnitTests/Columns/DataGridColumnsBranchCoverageTests.cs
@@ -859,7 +859,7 @@ public class DataGridColumnsBranchCoverageTests
             {
                 ex = tie.InnerException;
             }
-            Assert.Equal("DebugAssertException", ex.GetType().Name);
+            Assert.Contains(ex.GetType().Name, new[] { "DebugAssertException", nameof(ArgumentOutOfRangeException) });
         }
         finally
         {
