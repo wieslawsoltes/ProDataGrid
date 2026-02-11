42b1042c5d1053af49a28ad4a87872540312c0e3
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-11 23:18:26 +0100

    feat(theme): add tokenized metrics and density style resources

---
+++ src/Avalonia.Controls.DataGrid/Themes/DataGridDensityStyle.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+namespace Avalonia.Controls.Themes;
+
+/// <summary>
+/// Defines density presets for DataGrid theme resources.
+/// </summary>
+public enum DataGridDensityStyle
+{
+    /// <summary>
+    /// Uses default control sizing.
+    /// </summary>
+    Normal,
+
+    /// <summary>
+    /// Uses compact sizing for tighter layouts.
+    /// </summary>
+    Compact
+}
---
+++ src/Avalonia.Controls.DataGrid/Themes/DataGridFluentTheme.axaml.cs
@@ -0,0 +1,75 @@
+// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for details.
+
+using System;
+using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace Avalonia.Controls.Themes;
+
+/// <summary>
+/// Fluent DataGrid theme resource host with density switching support.
+/// </summary>
+public partial class DataGridFluentTheme : Styles, IResourceNode
+{
+    readonly ResourceDictionary _compactStyles;
+    DataGridDensityStyle _densityStyle;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DataGridFluentTheme"/> class.
+    /// </summary>
+    /// <param name="serviceProvider">Optional service provider used by XAML loader.</param>
+    public DataGridFluentTheme(IServiceProvider? serviceProvider = null)
+    {
+        AvaloniaXamlLoader.Load(serviceProvider, this);
+        _compactStyles = (ResourceDictionary)GetAndRemove("CompactStyles");
+
+        object GetAndRemove(string key)
+        {
+            var value = Resources[key] ?? throw new KeyNotFoundException($"Key '{key}' was not found in resources.");
+            Resources.Remove(key);
+            return value;
+        }
+    }
+
+    /// <summary>
+    /// Identifies the <see cref="DensityStyle"/> property.
+    /// </summary>
+    public static readonly DirectProperty<DataGridFluentTheme, DataGridDensityStyle> DensityStyleProperty =
+        AvaloniaProperty.RegisterDirect<DataGridFluentTheme, DataGridDensityStyle>(
+            nameof(DensityStyle),
+            o => o.DensityStyle,
+            (o, v) => o.DensityStyle = v);
+
+    /// <summary>
+    /// Gets or sets the density style used by this theme.
+    /// </summary>
+    public DataGridDensityStyle DensityStyle
+    {
+        get => _densityStyle;
+        set => SetAndRaise(DensityStyleProperty, ref _densityStyle, value);
+    }
+
+    /// <inheritdoc />
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == DensityStyleProperty)
+        {
+            Owner?.NotifyHostedResourcesChanged(new ResourcesChangedEventArgs());
+        }
+    }
+
+    bool IResourceNode.TryGetResource(object key, ThemeVariant? theme, out object? value)
+    {
+        if (_densityStyle == DataGridDensityStyle.Compact && _compactStyles.TryGetResource(key, theme, out value))
+        {
+            return true;
+        }
+
+        return base.TryGetResource(key, theme, out value);
+    }
+}
