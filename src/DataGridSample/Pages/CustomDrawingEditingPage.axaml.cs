62ad8df04290d1a232fdb620209d6ca632e59973
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-02-28 07:59:06 +0100

    Add editable custom drawing columns sample page

---
+++ src/DataGridSample/Pages/CustomDrawingEditingPage.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DataGridSample.Pages;
+
+public partial class CustomDrawingEditingPage : UserControl
+{
+    public CustomDrawingEditingPage()
+    {
+        InitializeComponent();
+    }
+}
