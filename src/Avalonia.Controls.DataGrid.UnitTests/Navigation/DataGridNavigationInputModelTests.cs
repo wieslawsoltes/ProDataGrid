d54f394707006c4252af972028c29f7a61438c54
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-08-13 15:09:18 +0200

    test: fix navigation input clean build coverage

--- src/Avalonia.Controls.DataGrid.UnitTests/Navigation/DataGridNavigationInputModelTests.cs
+++ src/Avalonia.Controls.DataGrid.UnitTests/Navigation/DataGridNavigationInputModelTests.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Controls.DataGridNavigation;
 using Avalonia.Controls.Primitives;
 using Avalonia.Data;
+using Avalonia.Headless;
 using Avalonia.Headless.XUnit;
 using Avalonia.Input;
 using Avalonia.Input.Raw;
